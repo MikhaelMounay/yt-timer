b1726a82f791aca0e4b4262e9b47eae1414873cc
mikhaelmounay <mikhaelmounay@gmail.com>
2024-12-13 11:19:46 +0200

    BUG-FIX: extension won't initialize correctly if navigated to youtube.com/watch from another page not through direct URL

--- src/main.js
+++ src/main.js
@@ -1,35 +1,53 @@
 let IS_PLAYING = true;
 let LIMIT_EXCEEDED = false;
 
-const video = document.getElementsByTagName("video")[0];
-const leftControlsDiv = document.getElementsByClassName("ytp-left-controls")[0];
-
 const separatorNode = document.createElement("div");
 separatorNode.innerText = "|";
 separatorNode.classList.add("ytp-time-display", "ytp-time-duration");
-leftControlsDiv.appendChild(separatorNode);
 
 const ytTimerDiv = document.createElement("div");
 ytTimerDiv.classList.add("ytp-time-display", "ytp-time-duration");
 updateTimer(0);
-leftControlsDiv.appendChild(ytTimerDiv);
 
-video.onplaying = function () {
-    if (LIMIT_EXCEEDED) {
-        video.pause();
-        IS_PLAYING = false;
-        limitAlert(LIMIT_s);
-    } else {
-        IS_PLAYING = true;
+let lastUrl = location.href;
+new MutationObserver(function () {
+    const url = location.href;
+    if (url !== lastUrl) {
+        lastUrl = url;
+        initialize();
     }
-};
+}).observe(document, { subtree: true, childList: true });
 
-video.onpause = function () {
-    IS_PLAYING = false;
-};
+initialize();
+
+function initialize() {
+    const video = document.getElementsByTagName("video")[0];
+    const leftControlsDiv = document.getElementsByClassName("ytp-left-controls")[0];
+
+    if (location.href.includes("youtube.com/watch")) {
+        leftControlsDiv.appendChild(separatorNode);
+        leftControlsDiv.appendChild(ytTimerDiv);
+    }
+
+    if (video) {
+        video.onplaying = function () {
+            if (LIMIT_EXCEEDED) {
+                video.pause();
+                IS_PLAYING = false;
+                limitAlert(LIMIT_s);
+            } else {
+                IS_PLAYING = true;
+            }
+        };
+
+        video.onpause = function () {
+            IS_PLAYING = false;
+        };
+    }
+}
 
 setInterval(function () {
-    if (IS_PLAYING) {
+    if (location.href.includes("youtube.com/watch") && IS_PLAYING) {
         chrome.storage.local.get(["moments"], function (items) {
             if (items.moments === undefined) {
                 chrome.storage.local.set({ moments: [`${Date.now()}`] });
